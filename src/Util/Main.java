2def5f4c185f8288f603482e5f54063487de3ebd
José Peterson Menezes Machado <78889539+PetsuTHEPRO@users.noreply.github.com>
2024-04-16 10:14:22 -0300

    improve

--- src/Util/Main.java
+++ src/Util/Main.java
@@ -1,94 +1,89 @@
-package Util;
-
-import Estruturas.DoublyLinkedList;
-import Estruturas.Queue;
-import Estruturas.Stack;
-import Estruturas.Tree;
+package util;
 
 import java.util.List;
+import java.util.Scanner;
+
+import estruturas.DoublyLinkedList;
+import estruturas.EstruturaDeDados;
+import estruturas.Queue;
+import estruturas.Stack;
+import estruturas.Tree;
+import manipulacaoarquivo.ManipulacaoArquivo;
 
 public class Main {
 
     public static void main(String[] args) {
-
         ManipulacaoArquivo ma = new ManipulacaoArquivo();
         EstruturaDeDados<String> estrutura = null;
-        
+        Scanner scanner = new Scanner(System.in);
+
         List<String> lines = ma.lerArquivo("src/Arquivos/exec.txt");
         List<String> datas = ma.lerArquivo("src/Arquivos/dados.txt");
 
         String estruturaDeDados = lines.remove(0);
 
-        switch (estruturaDeDados){
+        switch (estruturaDeDados) {
             case "lista":
                 System.out.println("Lista");
-                estrutura = new DoublyLinkedList<>(datas);
+                estrutura = new DoublyLinkedList<>();
                 break;
             case "pilha":
                 System.out.println("Pilha");
-                estrutura = new Stack<>(datas);
+                estrutura = new Stack<>();
                 break;
             case "fila":
                 System.out.println("Fila");
-                estrutura = new Queue<>(datas);
+                estrutura = new Queue<>();
                 break;
             case "arvore":
                 System.out.println("Arvore");
-                estrutura = new Tree<>(datas);
+                estrutura = new Tree<>();
                 break;
             default:
                 System.out.println("Estrutura errada [lista/pilha/fila/arvore]");
-                break;
+                return;
         }
 
-        for(String line : lines){
+        estrutura.addAll(datas);
 
-            if(estrutura == null){
-                throw new RuntimeException("Estrutura não inicializada.");
+        for (String line : lines) {
+            if (estrutura == null) {
+                System.out.println("Estrutura não inicializada.");
+                scanner.close();
+                return;
             }
 
-            if(line.contains(";")){
-                String[] operacao = line.split(";");
-                String comando = operacao[0], dado = operacao[1];
+            String[] operacao = line.split(";");
+            String comando = operacao[0].toLowerCase(), dado = operacao[1];
 
-                switch(comando){
-                    case "INSERIR":
-                        estrutura.add(dado);
-                        if(dado != null){
-                            System.out.println("INSERIR -> " + dado);
-                        }
-                        break;
-                    case "REMOVER":
-                        String dadoRemovido = estrutura.remove(dado);
-                        if(dado != null){
-
-                            if(dadoRemovido == null){
-                                System.out.println("REMOVER -> Item não está na lista: " + dado);
-                            }else{
-                                System.out.println("REMOVIDO -> " + dadoRemovido);
-                            }
-
-                        }else{
-                            System.out.println("REMOVER: Item não encontrado! [" + dadoRemovido + "]");
-                        }
-                        break;
-                    case "BUSCAR":
-                        if(estrutura.seek(dado)){
-                            System.out.println("BUSCAR -> Encontrou: " + dado);
-                        }else{
-                            System.out.println("BUSCAR -> Não Encontrou: " + dado);
-                        }
-                        break;
-                }
-            }else {
-                if(line.equals("IMPRIMIR")){
+            switch (comando) {
+                case "inserir":
+                    estrutura.add(dado);
+                    System.out.println("INSERIDO -> " + dado);
+                    break;
+                case "remover":
+                    if (estrutura.remove(dado)) {
+                        System.out.println("REMOVIDO -> " + dado);
+                    } else {
+                        System.out.println("REMOVER -> Item não encontrado: " + dado);
+                    }
+                    break;
+                case "buscar":
+                    if (estrutura.seek(dado)) {
+                        System.out.println("BUSCAR -> Encontrou: " + dado);
+                    } else {
+                        System.out.println("BUSCAR -> Não encontrou: " + dado);
+                    }
+                    break;
+                case "imprimir":
                     System.out.println("IMPRIMIR -> " + estrutura.print());
-                }else{
+                    break;
+                default:
                     System.out.println("Comando não existe!");
-                }
             }
         }
-        
+
+        scanner.close();
     }
 
 }
