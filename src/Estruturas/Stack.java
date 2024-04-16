172914ff98446a60aea882152971e49b72fa9a7e
José Peterson Menezes Machado <78889539+PetsuTHEPRO@users.noreply.github.com>
2024-04-16 10:14:18 -0300

    improve

--- src/Estruturas/Stack.java
+++ src/Estruturas/Stack.java
@@ -1,70 +1,64 @@
 package Estruturas;
 
 import Util.EstruturaDeDados;
-
 import java.util.EmptyStackException;
 import java.util.List;
 
-class NodoSimples<T>{
+class Node<T> {
+    private T element;
+    private Node<T> next;
 
-    private T elemento;
-    private NodoSimples<T> proximo;
-
-    public NodoSimples(){
-        this.elemento = null;
-        this.proximo = null;
-    }
-    public T getElemento() {
-        return elemento;
+    public Node(T element, Node<T> next) {
+        this.element = element;
+        this.next = next;
     }
 
-    public void setElemento(T elemento) {
-        this.elemento = elemento;
+    public T getElement() {
+        return element;
     }
 
-    public NodoSimples<T> getProximo() {
-        return proximo;
+    public void setElement(T element) {
+        this.element = element;
     }
 
-    public void setProximo(NodoSimples<T> proximo) {
-        this.proximo = proximo;
+    public Node<T> getNext() {
+        return next;
     }
 
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
+
 public class Stack<T> implements EstruturaDeDados<T> {
 
-    private NodoSimples<T> top;
-    private int tamanho;
+    private Node<T> top;
+    private int size;
 
-    public Stack(List<T> dados){
+    public Stack() {
         this.top = null;
-        this.criarEstrutura(dados);
+        this.size = 0;
+    }
+
+    public Stack(List<T> data) {
+        this();
+        this.createStructure(data);
     }
 
     @Override
-    public void criarEstrutura(List<T> dados) {
-        for(T dado : dados){
+    public void createStructure(List<T> data) {
+        for (T dado : data) {
             push(dado);
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return top == null;
     }
 
-    public void add(T elemento) {
-        push(elemento);
-    }
-
-    public void push(T elemento) {
-        NodoSimples<T> novoNodo = new NodoSimples<>();
-        novoNodo.setElemento(elemento);
-
-        if (!isEmpty()) {
-            novoNodo.setProximo(top);
-        }
-        top = novoNodo;
-        this.tamanho++;
+    public void push(T element) {
+        top = new Node<>(element, top);
+        size++;
     }
 
     public T pop() {
@@ -72,44 +66,64 @@ public class Stack<T> implements EstruturaDeDados<T> {
             throw new EmptyStackException();
         }
 
-        T elementoRemovido = top.getElemento();
-        top = top.getProximo();
-        this.tamanho--;
+        T elementRemoved = top.getElement();
+        top = top.getNext();
+        size--;
 
-        return elementoRemovido;
+        return elementRemoved;
     }
 
-    public T remove(T elemento) {
-        return pop();
+    public T remove(T element) {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+
+        if (top.getElement().equals(element)) {
+            return pop();
+        }
+
+        Node<T> current = top;
+        while (current.getNext() != null && !current.getNext().getElement().equals(element)) {
+            current = current.getNext();
+        }
+
+        if (current.getNext() == null) {
+            throw new RuntimeException("Element not found in the stack");
+        }
+
+        T elementRemoved = current.getNext().getElement();
+        current.setNext(current.getNext().getNext());
+        size--;
+
+        return elementRemoved;
     }
 
-    public boolean seek(T elemento) {
-        NodoSimples<T> nodoAtual = top;
-
-        while (nodoAtual != null) {
-            T elementoAtual = nodoAtual.getElemento();
-            if (elementoAtual.equals(elemento)) {
+    public boolean seek(T element) {
+        Node<T> current = top;
+        while (current != null) {
+            if (current.getElement().equals(element)) {
                 return true;
             }
-            nodoAtual = nodoAtual.getProximo();
+            current = current.getNext();
         }
 
         return false;
     }
 
-    public StringBuilder print(){
-
+    public StringBuilder print() {
         StringBuilder sb = new StringBuilder();
-        NodoSimples<T> nodoAtual = top;
-
         sb.append("[");
-        while (nodoAtual != null) {
-            sb.append(nodoAtual.getElemento()).append(" ");
-            nodoAtual = nodoAtual.getProximo();
+        Node<T> current = top;
+        while (current != null) {
+            sb.append(current.getElement()).append(" ");
+            current = current.getNext();
         }
         sb.append("]");
 
         return sb;
     }
 
+    public int size() {
+        return size;
+    }
 }
